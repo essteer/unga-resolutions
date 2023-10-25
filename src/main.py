5c2221d7458fe9223fc7fc92d25ad9174bc56dbf
essteer <essteer@pm.me>
2023-10-25 11:57:16 +0800

    restructure functions and complete link code

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ LANGUAGE = "?ln=en"
 
 # Master dict for resolution dicts
 resolutions_dict = {}
-# Cannot paginate >500 records, so iterate by year (~100 per year)
+# Cannot paginate >500 records, so iterate by year (~100 records per year)
 sessions_list = list(range(1946, PRESENT_SESSION + 1))
 # No records available for 1964
 sessions_list.remove(1964)
@@ -57,8 +57,22 @@ USER_AGENTS = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KH
                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36", 
                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36", 
                "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0", 
-               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"]
-USER_AGENT_PROBS = [0.3, 0.15, 0.12, 0.1, 0.1, 0.08, 0.05, 0.05, 0.05]
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36", 
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/118.0", 
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15", 
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0", 
+               "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0", 
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0", 
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60", 
+               "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/118.0", 
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.46", 
+               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15", 
+               "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36", 
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.47"]
+USER_AGENT_PROBS = [0.205, 0.14, 0.13, 0.105, 0.055, 
+                    0.055, 0.05, 0.045, 0.04, 0.03, 
+                    0.025, 0.02, 0.015, 0.015, 0.015, 
+                    0.0125, 0.0125, 0.012, 0.01, 0.008]
 
 header = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
@@ -74,68 +88,39 @@ header = {
   }
 
 ##########################################################################
-# Extract record links
+# Helper functions
 ##########################################################################
 
-# Assign weighted-random referer and user-agent to header
-referer = random.choices(REFERERS, weights=REFERER_PROBS, k=1)
-user_agent = random.choices(USER_AGENTS, weights=USER_AGENT_PROBS, k=1)
-header["Referer"] = referer[0]
-header["User-Agent"] = user_agent[0]
+def weighted_random_selection(sample_space: list[str], probs: list[float]) -> str:
+    """
+    Args:
+        sample_space: list of options to randomly select from
+        probs: list of probabilites per sample in sample_space (sum == 1)
+    Returns:
+        weighted random selection from sample_space
+    """
+    weighted_random_selection = random.choices(sample_space, weights=probs, k=1)
+    
+    return weighted_random_selection[0]
 
-search_html = requests.get(search_URL, headers=header)
-# Extract html source code
-search_URL_source_code = search_html.text
-# Parse source code
-raw_search = BeautifulSoup(search_URL_source_code, "html.parser")
 
-links = [link.get("href") for link in raw_search.find_all("a", href=True)]
+def get_segments(raw_data: str) -> list:
+    """
+    Extracts URL segments from a string object
+    
+    Args:
+        raw_data, string object
+    Returns:
+        segments, list of URL segments parsed from raw_data
+    """
+    segments = re.findall(r"record/(\d+)\?", raw_data)
+    return segments
 
-##########################################################################
-# Extract record data
-##########################################################################
-
-# Assign weighted-random referer and user-agent to header
-referer = random.choices(REFERERS, weights=REFERER_PROBS, k=1)
-user_agent = random.choices(USER_AGENTS, weights=USER_AGENT_PROBS, k=1)
-header["Referer"] = referer
-header["User-Agent"] = user_agent
-
-# Example record URL "https://digitallibrary.un.org/record/4016932?ln=en"
-# record_URL = BASE_URL_RECORD + "4016932" + LANGUAGE
-# record_html = requests.get(record_URL, headers=header)
-# # Extract html source code
-# record_URL_source_code = record_html.text
-# # Parse source code
-# raw_record = BeautifulSoup(record_URL_source_code, "html.parser")
-
-##########################################################################
-# Process data
-##########################################################################
-"""
-Voting data is contained in spans with the class: 
-class="value col-xs-12 col-sm-9 col-md-10".
-"""
-# data = raw_record.find_all("span", class_="value col-xs-12 col-sm-9 col-md-10")
-# Check object indices in data:
-# for i in range(len(data)):
-#     print(f"data[{i}]: \n {data[i]}")
-
-# ~~~ Extract text into variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-title = data[0].text
-agenda = data[1].text
-resolution = data[2].text
-meeting_record = data[3].text
-draft_resolution = data[4].text
-note = data[5].text
-voting_summary = data[6].text
-vote_date = data[7].text
-
-# ~~~ Parse voting figures ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def get_figures(raw_data: str) -> list:
     """
+    Extracts a list of string integers within a string object
+    
     Args:
         raw_data, string object
     Returns:
@@ -145,75 +130,154 @@ def get_figures(raw_data: str) -> list:
     return figures
 
 
-"""voting_summary structure
-Voting Summary Yes: 80 | No: 2 | Abstentions: 47 | Non-Voting: 64 | Total voting membership: 193 '
-"""
-voting_figures = get_figures(voting_summary)
-
-
-# ~~~ Sort countries by vote record ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-def sort_countries(raw_data: ResultSet) -> None:
-    """
-    Separates countries based on voting record
-    Appends countries to corresponding lists
+# def sort_countries(raw_data: ResultSet) -> None:
+#     """
+#     Sorts countries based on voting record
+#     Appends countries to corresponding lists
     
-    Args:
-        raw_data, BeautifulSoup bs4.element.ResultSet object    
-    Returns:
-        None
-    """
-    raw = raw_data[:]
+#     Args:
+#         raw_data, BeautifulSoup bs4.element.ResultSet object    
+#     Returns:
+#         None
+#     """
+#     raw = raw_data[:]
     
-    for i in range(len(raw)):
-        # Separate vote and name values
-        split_values = raw[i].split(maxsplit=1)
-        # Yes votes
-        if split_values[0] == "Y":
-            yes_countries.append(split_values[1])
-        # No votes
-        elif split_values[0] == "N":
-            no_countries.append(split_values[1])
-        # Abstentions
-        elif split_values[0] == "A":
-            abstention_countries.append(split_values[1])
-        # Non-voting
-        else:
-            non_voting_countries.append(raw[i].strip())
+#     for i in range(len(raw)):
+#         # Separate vote and name values
+#         split_values = raw[i].split(maxsplit=1)
+#         # Yes votes
+#         if split_values[0] == "Y":
+#             yes_countries.append(split_values[1])
+#         # No votes
+#         elif split_values[0] == "N":
+#             no_countries.append(split_values[1])
+#         # Abstentions
+#         elif split_values[0] == "A":
+#             abstention_countries.append(split_values[1])
+#         # Non-voting
+#         else:
+#             non_voting_countries.append(raw[i].strip())
+
+
+##########################################################################
+# Extract record links
+##########################################################################
+
+# Get weighted random selection of referer and user-agent
+referer = weighted_random_selection(REFERERS, REFERER_PROBS)
+user_agent = weighted_random_selection(USER_AGENTS, USER_AGENT_PROBS)
+
+# for i in range(10):
+#     referer = weighted_random_selection(REFERERS, REFERER_PROBS)
+#     user_agent = weighted_random_selection(USER_AGENTS, USER_AGENT_PROBS)
+#     print(f"Referer:    {referer}")
+#     print(f"User-agent: {user_agent}\n")
+
+# Assign weighted random referer and user-agent to header
+header["Referer"] = referer
+header["User-Agent"] = user_agent
+
+search_html = requests.get(search_URL, headers=header)
+# Extract html source code
+search_URL_source_code = search_html.text
+# Parse source code
+raw_search = BeautifulSoup(search_URL_source_code, "html.parser")
+
+# ~~~ Parse record page links ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Get link elements from source code
+links = str(raw_search.find_all("a", href=True))
+# Get URL segments and remove duplicates
+segments = list(set(get_segments(links)))
+
+##########################################################################
+# Iterate through segments
+##########################################################################
+# TODO uncomment from here
+# for segment in segments[:2]:
     
+#     ##########################################################################
+#     # Extract record data
+#     ##########################################################################
+    
+#     # Get weighted random selection of referer and user-agent
+#     referer = weighted_random_selection(REFERERS, REFERER_PROBS)
+#     user_agent = weighted_random_selection(USER_AGENTS, USER_AGENT_PROBS)
+#     # Assign weighted random referer and user-agent to header
+#     header["Referer"] = referer
+#     header["User-Agent"] = user_agent
+    
+#     # Example record URL "https://digitallibrary.un.org/record/4016932?ln=en"
+#     record_URL = BASE_URL_RECORD + segment + LANGUAGE
+#     record_html = requests.get(record_URL, headers=header)
+#     # Extract html source code
+#     record_URL_source_code = record_html.text
+#     # Parse source code
+#     raw_record = BeautifulSoup(record_URL_source_code, "html.parser")
+    
+#     ##########################################################################
+#     # Process data
+#     ##########################################################################
+#     """
+#     Voting data is contained in spans with the class: 
+#     class="value col-xs-12 col-sm-9 col-md-10".
+#     """
+#     data = raw_record.find_all("span", class_="value col-xs-12 col-sm-9 col-md-10")
+#     # Check object indices in data:
+#     # for i in range(len(data)):
+#     #     print(f"data[{i}]: \n {data[i]}")
+    
+#     # ~~~ Extract text into variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     # TODO make this dynamic, cannot rely on fixed order
+#     # TODO make variables == "" or "None" when no record found
+#     title = data[0].text
+#     agenda = data[1].text
+#     resolution = data[2].text
+#     meeting_record = data[3].text
+#     draft_resolution = data[4].text
+#     note = data[5].text
+#     voting_summary = data[6].text
+#     vote_date = data[7].text
+    
+#     # ~~~ Sort countries by vote record ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     """
+#     Voting Summary Yes: 80 | No: 2 | Abstentions: 47 | Non-Voting: 64 | Total voting membership: 193 '
+#     """
+#     voting_figures = get_figures(voting_summary)
+    
+#     # Lists to store country names by voting record
+#     yes_countries = []
+#     no_countries = []
+#     abstention_countries = []
+#     non_voting_countries = []
+    
+#     # Extract vote and country string from data
+#     vote_by_country = data[8].find_all(string=True)
+#     # Sort countries into lists by vote record
+#     sort_countries(vote_by_country)
+    
+#     # ~~~ Create dict of resolution data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+#     # Set record_name to current resolution reference
+#     record_name = str(resolution)
+    
+#     # Add dict of current resolution to main resolutions_dict
+#     resolutions_dict[record_name] = {"Title": title, 
+#                                      "Agenda": agenda,
+#                                      "Resolution": resolution, 
+#                                      "Meeting Record": meeting_record, 
+#                                      "Draft Resolution": draft_resolution, 
+#                                      "Note": note, 
+#                                      "Num Yes": int(voting_figures[0]), 
+#                                      "Num No": int(voting_figures[1]), 
+#                                      "Num Abstentions": int(voting_figures[2]), 
+#                                      "Num Non-Voting": int(voting_figures[3]), 
+#                                      "Total Votes": int(voting_figures[4]), 
+#                                      "Vote Date": vote_date, 
+#                                      "Yes Votes": yes_countries, 
+#                                      "No Votes": no_countries, 
+#                                      "Abstentions": abstention_countries, 
+#                                      "Non-Voting": non_voting_countries, 
+#                                      "Record URL": str(record_URL)
+#                                      }
 
-# Lists to store country names by voting record
-yes_countries = []
-no_countries = []
-abstention_countries = []
-non_voting_countries = []
-
-# Extract vote and country string from data
-vote_by_country = data[8].find_all(string=True)
-# Sort countries into lists by vote record
-sort_countries(vote_by_country)
-
-
-# ~~~ Create dict of resolution data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# Set record_name to current resolution reference
-record_name = str(resolution)
-
-# Add dict of current resolution to main resolutions_dict
-resolutions_dict[record_name] = {"Title": title, 
-                                 "Agenda": agenda,
-                                 "Resolution": resolution, 
-                                 "Meeting Record": meeting_record, 
-                                 "Draft Resolution": draft_resolution, 
-                                 "Note": note, 
-                                 "Num Yes": int(voting_figures[0]), 
-                                 "Num No": int(voting_figures[1]), 
-                                 "Num Abstentions": int(voting_figures[2]), 
-                                 "Num Non-Voting": int(voting_figures[3]), 
-                                 "Total Votes": int(voting_figures[4]), 
-                                 "Vote Date": vote_date, 
-                                 "Yes Votes": yes_countries, 
-                                 "No Votes": no_countries, 
-                                 "Abstentions": abstention_countries, 
-                                 "Non-Voting": non_voting_countries
-                                 }
