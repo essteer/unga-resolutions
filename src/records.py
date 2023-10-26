eb32de0567cda7ef7394b2fd95003cddf9f54d37
essteer <essteer@pm.me>
2023-10-27 01:52:29 +0800

    update records loop and file save code

--- src/records.py
+++ src/records.py
@@ -3,6 +3,12 @@ import csv, random, re, requests, time
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet
 from datetime import datetime
+import os
+
+# TODO: generalise get_figures to handle missing numbers
+
+# NOTE: update before running
+LATEST_VERSION = "./data/20231025_records.csv"
 
 ##########################################################################
 # Prepare URLs and requests
@@ -15,12 +21,21 @@ LANGUAGE = "?ln=en"
 # Store URL segments in list
 SEGMENTS = []
 # Read csv of link segments
-with open("./data/20231026_link_segments.csv", "r") as file:
+filename = "./data/20231026_link_segments.csv"
+with open(filename, "r") as file:
     csv_reader = csv.reader(file)
     # Skip header row
     header = next(csv_reader)
     for row in csv_reader:
         SEGMENTS.append(row[0])
+
+# Read csv of link segments previously processed
+with open("./data/processed_segments.txt", "r") as file:
+    processed_segments = [line.strip() for line in file]
+    
+BATCH_SIZE = 20
+START = len(processed_segments)
+END = min(START + BATCH_SIZE, len(SEGMENTS)) + 1
 
 # Time delays in seconds
 MIN_DELAY = 2
@@ -128,27 +143,28 @@ def sort_countries(raw_data: ResultSet) -> None:
             non_voting_countries.append(raw[i].strip())
 
 
-
 ##########################################################################
 # Iterate through records
 ##########################################################################
 
 # Master dict for resolution records
 resolutions_dict = {}
+# Set counter
+counter = 0
 
-test_segments = ["4016932", "3839868"]
+test_segments = ["4016932", "3839868", "671259"]
 for segment in test_segments:
 
-# for segment in SEGMENTS[:2]:
+# for segment in SEGMENTS[START:END]:
+
+    if segment in processed_segments:
+        print(f"segment {segment} already processed")
+        continue
     
     # Generate random delay
     random_delay = random.uniform(MIN_DELAY, MAX_DELAY)
     # Add random delay
     time.sleep(random_delay)
-    
-    ##########################################################################
-    # Extract record data
-    ##########################################################################
     
     # Get weighted random selection of referer and user-agent
     referer = weighted_random_selection(REFERERS, REFERER_PROBS)
@@ -165,85 +181,112 @@ for segment in test_segments:
     # Parse source code
     raw_record = BeautifulSoup(record_URL_source_code, "html.parser")
     
-    ##########################################################################
-    # Process data
-    ##########################################################################
-    
     # Voting metadata contained in multiple <div class="metadata-row">
     keys = raw_record.find_all("span", class_="title col-xs-12 col-sm-3 col-md-2")
     values = raw_record.find_all("span", class_="value col-xs-12 col-sm-9 col-md-10")
-    
-    # ~~~ Extract text into variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
+    # Create dict of metadata
     metadata_dict = {}
-    
     for key, value in zip(keys, values):
         metadata_dict[key.text.strip()] = value.text
     
-    # title = values[0].text
-    # agenda = values[1].text
-    # resolution = values[2].text
-    # meeting_record = values[3].text
-    # draft_resolution = values[4].text
-    # committee_report = 1
-    # note = values[5].text
-    # voting_summary = values[6].text
-    # vote_date = values[7].text
-    
-    title =             str(metadata_dict.get("Title", "None"))
-    agenda =            str(metadata_dict.get("Agenda", "None"))
-    resolution =        str(metadata_dict.get("Resolution", "None"))
-    meeting_record =    str(metadata_dict.get("Meeting record", "None"))
-    draft_resolution =  str(metadata_dict.get("Draft resolution", "None"))
-    committee_report =  str(metadata_dict.get("Committee report", "None"))
-    note =              str(metadata_dict.get("Note", "None"))
-    vote_date =         str(metadata_dict.get("Vote date", "None"))
-    
-    # ~~~ Sort countries by vote record ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    """
-    Voting Summary Yes: 80 | No: 2 | Abstentions: 47 | Non-Voting: 64 | Total voting membership: 193 '
-    """
+    # Extract vote figures
     try:
         voting_summary = metadata_dict["Vote summary"]
         voting_figures = get_figures(voting_summary)
     except KeyError:
         voting_figures = ["0", "0", "0", "0", "0"]
     
-    # Lists to store country names by voting record
-    yes_countries = []
-    no_countries = []
-    abstention_countries = []
-    non_voting_countries = []
     
-    # Extract vote and country string from data
-    vote_by_country = values[-2].find_all(string=True)  # TODO update this based on key location
-    # Sort countries into lists by vote record
-    sort_countries(vote_by_country)
+    try:
+        # Extract vote and country string from data
+        key_list = [key.text.strip() for key in keys]
+        vote_index = key_list.index("Vote")
+        vote_by_country = values[vote_index].find_all(string=True)
+        # Lists to store country names by voting record
+        yes_countries = []
+        no_countries = []
+        abstention_countries = []
+        non_voting_countries = []
+        # Sort countries into lists by vote record
+        sort_countries(vote_by_country)
+    except ValueError:
+        yes_countries = ["Unknown"]
+        no_countries = ["Unknown"]
+        abstention_countries = ["Unknown"]
+        non_voting_countries = ["Unknown"]
     
-    # ~~~ Create dict of resolution data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
-    # Set record_name to current resolution reference
-    record_name = str(resolution)
-    
+    # Set record_name to resolution reference
+    resolution = str(metadata_dict.get("Resolution", "None"))
     # Add dict of current resolution to main resolutions_dict
-    resolutions_dict[record_name] = {"Title": title, 
-                                     "Agenda": agenda,
-                                     "Resolution": resolution, 
-                                     "Meeting Record": meeting_record, 
-                                     "Draft Resolution": draft_resolution, 
-                                     "Committee Report": committee_report,
-                                     "Note": note, 
-                                     "Num Yes": int(voting_figures[0]), 
-                                     "Num No": int(voting_figures[1]), 
-                                     "Num Abstentions": int(voting_figures[2]), 
-                                     "Num Non-Voting": int(voting_figures[3]), 
-                                     "Total Votes": int(voting_figures[4]), 
-                                     "Vote Date": vote_date, 
-                                     "Yes Votes": yes_countries, 
-                                     "No Votes": no_countries, 
-                                     "Abstentions": abstention_countries, 
-                                     "Non-Voting": non_voting_countries, 
-                                     "Record URL": str(record_URL)
-                                     }
+    resolutions_dict[resolution] = {"Resolution": resolution,
+                                    "Vote Date": str(metadata_dict.get("Vote date", "None")), 
+                                    "Num Yes": int(voting_figures[0]), 
+                                    "Num No": int(voting_figures[1]), 
+                                    "Num Abstentions": int(voting_figures[2]), 
+                                    "Num Non-Voting": int(voting_figures[3]), 
+                                    "Total Votes": int(voting_figures[4]), 
+                                    "Record URL": str(record_URL),
+                                    "Segment": segment,
+                                    "Title": str(metadata_dict.get("Title", "None")), 
+                                    "Yes Votes": yes_countries, 
+                                    "No Votes": no_countries, 
+                                    "Abstentions": abstention_countries, 
+                                    "Non-Voting": non_voting_countries, 
+                                    "Agenda": str(metadata_dict.get("Agenda", "None")),
+                                    "Meeting Record": str(metadata_dict.get("Meeting record", "None")), 
+                                    "Draft Resolution": str(metadata_dict.get("Draft resolution", "None")), 
+                                    "Committee Report": str(metadata_dict.get("Committee report", "None")),
+                                    "Note": str(metadata_dict.get("Note", "None"))
+                                    }
+    
+    processed_segments.append(segment)
+    counter += 1
 
-print("Process complete.")
+##########################################################################
+# Save to csv
+##########################################################################
+
+# Get current date in the format "yyyymmdd"
+today = datetime.now().strftime("%Y%m%d")
+# Set filename
+new_version = f"./data/{today}_records.csv"
+
+# Check whether the file exists
+file_exists = os.path.isfile(LATEST_VERSION)
+
+if file_exists:
+
+    # Read the existing CSV file
+    with open(LATEST_VERSION, "r", newline="") as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    # Append resolutions_dict data to existing data
+    for res, metadata in resolutions_dict.items():
+        new_row = [metadata.get(column, "") for column in data[0]]
+        data.append(new_row)
+
+    # Save entire dataset to new file
+    with open(new_version, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+  
+else:
+    # Save resolutions_dict data to csv
+    with open(new_version, "w", newline="") as file:
+        
+        columns = list(resolutions_dict.values())[0].keys()
+        writer = csv.DictWriter(file, fieldnames=columns)
+        
+        if not file_exists:
+            writer.writeheader()
+        
+        for res, metadata in resolutions_dict.items():
+            writer.writerow(metadata)
+
+# Updated file of processed URLs for later resumption
+with open("./data/processed_segments.txt", "a") as file:
+    for segment in processed_segments:
+        file.write(f"{segment}\n")
+        
+print(f"{counter} record{'s' if counter != 1 else ''} saved to csv")
