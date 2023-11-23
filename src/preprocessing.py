f7112564a6e363da695eb850bc6c96c23c267e88
essteer <essteer@pm.me>
2023-11-23 18:39:23 +0800

    add function to process country names in place

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -69,14 +69,13 @@ df = df.reset_index(drop=True)
 # Format country entries and get list of unique countries
 ##########################################################################
 
-def process_vote_column(vote_column):
+def process_vote_column(column):
     """
-    Formats the country names in vote record columns
-    Extends the set of unique country names
-    Returns the formatted column to the dataframe
+    Formats country names in vote record columns
+    Extends set of unique country names
+    Returns formatted column to the dataframe
     """
-    # Split, strip, and filter out empty values
-    formatted_column = [country.strip(" '[]") for country in vote_column.split(",") if country.strip(" '[]") != ""]
+    formatted_column = [c.strip(" '[]") for c in column.split(",") if c.strip(" '[]") != ""]
     
     global countries
     countries.update(formatted_column)
@@ -94,66 +93,83 @@ for vote_category in vote_categories:
 ##########################################################################
 # Consolidate country names
 ##########################################################################
-"""
-NOTE: use a merge function later to consolidate vote records
-"""
-aliases = {
-    'BYELORUSSIAN SSR': "BELARUS", 
-    'BOLIVIA (PLURINATIONAL STATE OF)': "BOLIVIA", 
-    'BRUNEI DARUSSALAM': "BRUNEI", 
-    'CABO VERDE': "CAPE VERDE", 
-    'CENTRAL AFRICAN EMPIRE': "CENTRAL AFRICAN REPUBLIC", 
-    'CEYLON': "SRI LANKA", 
-    'CONGO': "CONGO (ROC)", 
-    'CONGO (BRAZZAVILLE)': "CONGO (ROC)", 
-    'CONGO (DEMOCRATIC REPUBLIC OF)': "CONGO (DRC)",
-    'CONGO (LEOPOLDVILLE)': "CONGO (DRC)", 
-    '"CÔTE D\'IVOIRE"': "IVORY COAST", 
-    '"COTE D\'IVOIRE"': "IVORY COAST", 
-    'CZECH REPUBLIC': "CZECHIA", 
-    'DAHOMEY': "BENIN", 
-    'DEMOCRATIC KAMPUCHEA': "CAMBODIA", 
-    '"DEMOCRATIC PEOPLE\'S REPUBLIC OF KOREA"': "NORTH KOREA", 
-    'DEMOCRATIC REPUBLIC OF THE CONGO': "CONGO (DRC)", 
-    'GERMAN DEMOCRATIC REPUBLIC': "EAST GERMANY", 
-    'FEDERAL REPUBLIC OF': "GERMANY", 
-    'KHMER REPUBLIC': "CAMBODIA", 
-    'IRAN (ISLAMIC REPUBLIC OF)': "IRAN", 
-    '"LAO PEOPLE\'S DEMOCRATIC REPUBLIC"': "LAOS", 
-    '"LAO PEOPLE\'s DEMOCRATIC REPUBLIC"': "LAOS", 
-    'LAO': "LAOS", 
-    'LIBYAN ARAB JAMAHIRIYA': "LIBYA", 
-    'LIBYAN ARAB REPUBLIC': "LIBYA", 
-    'MALDIVE ISLANDS': "MALDIVES", 
-    'MICRONESIA (FEDERATED STATES OF)': "MICRONESIA", 
-    'PHILIPPINE REPUBLIC': "PHILIPPINES", 
-    'REPUBLIC OF KOREA': "SOUTH KOREA", 
-    'REPUBLIC OF MOLDOVA': "MOLDOVA", 
-    'RUSSIAN FEDERATION': "RUSSIA", 
-    'SAINT CHRISTOPHER AND NEVIS': "ST KITTS AND NEVIS", 
-    'SIAM': "THAILAND", 
-    'SURINAM': "SURINAME", 
-    'SWAZILAND': "ESWATINI", 
-    'SYRIAN ARAB REPUBLIC': "SYRIA", 
-    'TANGANYIKA': "TANZANIA",   
-    'THE FORMER YUGOSLAV REPUBLIC OF MACEDONIA': "NORTH MACEDONIA", 
-    'TÜRKİYE': "TURKIYE", 
-    'TURKEY': "TURKIYE", 
-    'UKRAINIAN SSR': "UKRAINE", 
-    'UNION OF SOUTH AFRICA': "SOUTH AFRICA",
-    'UNITED REPUBLIC OF CAMEROON': "CAMEROON", 
-    'UNITED REPUBLIC OF TANZANIA': "TANZANIA", 
-    'UPPER VOLTA': "BURKINA FASO", 
-    'USSR': "RUSSIA", 
-    'VENEZUELA (BOLIVARIAN REPUBLIC OF)': "VENEZUELA", 
-    'VIET NAM': "VIETNAM", 
-    'ZAIRE': "CONGO (DRC)", 
-    'ZANZIBAR': "TANZANIA"
-}
 
-countries_renamed = [aliases[country] if country.upper() in aliases else country.upper() for country in countries]
-countries_renamed = list(set(countries_renamed))
-countries_renamed.sort()
+def process_country_names(column):
+    """
+    Removes "Zanzibar" entries as per README.md
+    Updates historic country names to modern equivalents
+    Returns formatted column to the dataframe
+    """
+    aliases = {
+        'BYELORUSSIAN SSR': "BELARUS", 
+        'BOLIVIA (PLURINATIONAL STATE OF)': "BOLIVIA", 
+        'BRUNEI DARUSSALAM': "BRUNEI", 
+        'CABO VERDE': "CAPE VERDE", 
+        'CENTRAL AFRICAN EMPIRE': "CENTRAL AFRICAN REPUBLIC", 
+        'CEYLON': "SRI LANKA", 
+        'CONGO': "CONGO (ROC)", 
+        'CONGO (BRAZZAVILLE)': "CONGO (ROC)", 
+        'CONGO (DEMOCRATIC REPUBLIC OF)': "CONGO (DRC)",
+        'CONGO (LEOPOLDVILLE)': "CONGO (DRC)", 
+        '"CÔTE D\'IVOIRE"': "IVORY COAST", 
+        '"COTE D\'IVOIRE"': "IVORY COAST", 
+        'CZECH REPUBLIC': "CZECHIA", 
+        'DAHOMEY': "BENIN", 
+        'DEMOCRATIC KAMPUCHEA': "CAMBODIA", 
+        '"DEMOCRATIC PEOPLE\'S REPUBLIC OF KOREA"': "NORTH KOREA", 
+        'DEMOCRATIC REPUBLIC OF THE CONGO': "CONGO (DRC)", 
+        'GERMAN DEMOCRATIC REPUBLIC': "EAST GERMANY", 
+        'FEDERAL REPUBLIC OF': "GERMANY", 
+        'KHMER REPUBLIC': "CAMBODIA", 
+        'IRAN (ISLAMIC REPUBLIC OF)': "IRAN", 
+        '"LAO PEOPLE\'S DEMOCRATIC REPUBLIC"': "LAOS", 
+        '"LAO PEOPLE\'s DEMOCRATIC REPUBLIC"': "LAOS", 
+        'LAO': "LAOS", 
+        'LIBYAN ARAB JAMAHIRIYA': "LIBYA", 
+        'LIBYAN ARAB REPUBLIC': "LIBYA", 
+        'MALDIVE ISLANDS': "MALDIVES", 
+        'MICRONESIA (FEDERATED STATES OF)': "MICRONESIA", 
+        'PHILIPPINE REPUBLIC': "PHILIPPINES", 
+        'REPUBLIC OF KOREA': "SOUTH KOREA", 
+        'REPUBLIC OF MOLDOVA': "MOLDOVA", 
+        'RUSSIAN FEDERATION': "RUSSIA", 
+        'SAINT CHRISTOPHER AND NEVIS': "ST KITTS AND NEVIS", 
+        'SIAM': "THAILAND", 
+        'SURINAM': "SURINAME", 
+        'SWAZILAND': "ESWATINI", 
+        'SYRIAN ARAB REPUBLIC': "SYRIA", 
+        'TANGANYIKA': "TANZANIA",   
+        'THE FORMER YUGOSLAV REPUBLIC OF MACEDONIA': "NORTH MACEDONIA", 
+        'TÜRKİYE': "TURKIYE", 
+        'TURKEY': "TURKIYE", 
+        'UKRAINIAN SSR': "UKRAINE", 
+        'UNION OF SOUTH AFRICA': "SOUTH AFRICA",
+        'UNITED REPUBLIC OF CAMEROON': "CAMEROON", 
+        'UNITED REPUBLIC OF TANZANIA': "TANZANIA", 
+        'UPPER VOLTA': "BURKINA FASO", 
+        'USSR': "RUSSIA", 
+        'VENEZUELA (BOLIVARIAN REPUBLIC OF)': "VENEZUELA", 
+        'VIET NAM': "VIETNAM", 
+        'ZAIRE': "CONGO (DRC)", 
+        'ZANZIBAR': "TANZANIA"
+    }
+    
+    filtered_col = [c for c in column if c != "ZANZIBAR"]
+    
+    new_col = [aliases[c] if c.upper() in aliases else c.upper() for c in filtered_col]
+    
+    global countries_renamed
+    countries_renamed.update(new_col)
+    
+    return new_col
+
+
+countries_renamed = set()
+vote_categories = ["Yes Votes", "No Votes", "Abstentions", "Non-Voting"]
+
+# Apply process_vote_column to format country names
+for vote_category in vote_categories:
+    df[vote_category] = df[vote_category].apply(process_country_names)
 
 ##########################################################################
 # 
